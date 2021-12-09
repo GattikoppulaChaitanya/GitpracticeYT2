7af779abd340c6e75e548ccbf39f108da97ffadb
ChaitanyaGattikoppul <chaitanya.gattikoppula@ratnaglobaltech.com>
2021-12-09 12:56:52 +0530

    Changes

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         println("second Commit")
         println("local changes")
         println("Change 2")
-        println("This line is merged")
 
     }
 }
