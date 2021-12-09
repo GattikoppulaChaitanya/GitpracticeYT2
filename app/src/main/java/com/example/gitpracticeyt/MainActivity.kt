3439bd67412fd649b27d3c44b66372c8028ae72a
ChaitanyaGattikoppul <chaitanya.gattikoppula@ratnaglobaltech.com>
2021-12-09 11:19:41 +0530

    fourth commit

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("second Commit")
-        today is gooddat
+        uhvkjbfgyuewbfhjbsdvdbvj
     }
 }
